2fc231b649e91844e9297e3b2477a7ac9ad9272e
bgdtc <theo.bogdan@hotmail.com>
2021-09-23 16:56:23 +0200

    up

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
+from logging import error
 import os
 import discord
+from discord import colour
 from discord.ext import commands
 import requests
 from dotenv import load_dotenv
@@ -12,14 +14,33 @@ client = commands.Bot(command_prefix="!")
 # print(os.environ.get('TOKEN'))
 TOKEN = os.environ.get('TOKEN')
 
-# client = discord.Client()
-arrr = "pirate-chain"
-btc = "bitcoin"
+
+client.remove_command("help")
+
+@client.group(invoke_without_command=True)
+async def help(ctx):
+    em = discord.Embed(title = "!help",colour=0xE5E242, description = "```Affiche ce panneau d'aide```")
+    em.add_field(name = "!help <arg>", value = "```Affiche l'aide de la commande```")
+    em.add_field(name = "!price <arg>", value = "```Affiche la valeur du Token en USD```")
+    em.add_field(name = "!blague", value = "```Je vous fait une blague de Chuck Norris```")
+    
+    await ctx.send(embed = em)
+
+
+
 
 @client.event 
 async def on_ready():
-    print(f'{client.user} connecté')
 
+    print(f'{client.user} connecté')
+    r = requests.get("https://api.coingecko.com/api/v3/coins/list")
+    re = json.loads(r.text)
+
+    ret =  re                                                                                                   
+    jsonString = json.dumps(ret)
+    jsonFile = open("data.json", "w")
+    jsonFile.write(jsonString)
+    jsonFile.close()
 @client.command(aliases= ['purge','delete'])
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount : int):
@@ -33,25 +54,35 @@ async def clear(ctx, amount : int):
 @client.command(name='price')
 async def arrrprice(ctx, arg):
     
-    r = requests.get("https://api.coingecko.com/api/v3/coins/list")
-    re = json.loads(r.text)
-    ret =  re                                                                                                   
-    jsonString = json.dumps(ret)
-    jsonFile = open("data.json", "w")
-    jsonFile.write(jsonString)
-    jsonFile.close()
-    if not arg: 
-        print("pas arg")
+    
+    with open('data.json') as file_object:
+         data = json.load(file_object)
+         for i in range(len(data)): e= print("Coins: ", data[i]["name"])
+         print(e)
+    
+    
     # for i in range(len(ret)): e =  print("Notation: ","coins: ", r.json()[i]["name"], "| |","Symboles: ", r.json()[i]["symbol"])
     # json.dump(e, "monfichier.json")
-    re = 0
-    re = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={arg}&vs_currencies=usd")
-    ret = re.json()
-    rete = ret[f"{arg}"]
-    retex = rete["usd"]
-    print(rete)
-    await ctx.send(f'```Le {arg} vaut actuellement:  {retex} $```')
-
+         re = 0
+         re = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={arg}&vs_currencies=usd")
+         ret = re.json()
+    # if (arg in data  ):
+    #     print("error")
+    #     await ctx.send("error")
+    #     await ctx.send("voici la liste des tokens: ")
+    #     for i in range(len(data)): await ctx.send(f'```{data[i]["name"]}```')
+        
+    # else:
+         data = ret
+         coin = f"{arg}" in data
+         print(coin)
+         if coin == True:
+               rete = ret[f"{arg}"]
+               retex = rete["usd"]
+               print(rete)
+               await ctx.send(f'```Le {arg} vaut actuellement:  {retex} $```')
+         else:
+             await ctx.send(f'{arg} n\'est pas dans la liste des Tokens (utilisez le nom complet et non le ticker )')
 @client.command(name='blague')
 async def blague(ctx):
     re = 0
