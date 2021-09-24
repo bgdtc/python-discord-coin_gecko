c5c65b57c7541733e721174bdecfeec923cb55f9
bgdtc <theo.bogdan@hotmail.com>
2021-09-24 16:36:48 +0200

    update nmap + twilio + insult etc

--- README.md
+++ README.md
@@ -1,46 +1,16 @@
 # python-discord-coin_gecko
 
-
-
-# python-discord-coingecko
-
-# Pour run le bot, dans votre terminal saissisez
-
-npx nodemon --exec "clear;python3" bot.py
-##### Dans un premier temps créer un environnement 
-
-python3.8 -m venv .env
-## ATTENTION
-##### Linux risque de vous demander d'installer venv
-
-apt install python3.8-venv
-##### Il faut donc relancer la commande pour créer l'environnement
-
-##### Activer en sourçant
-
-source .env/bin/activate
-##### Pour l'utilisation de l'API de Discord, on installe discord.py avec pip
-
-pip install -U discord.py
-##### Après cela, il faut se rendre sur le portail des développeurs de discord
+#### Setup 
+1. `git clone https://github.com/bgdtc/python-discord-coin_gecko`
+2. `cd python-discord-coin_gecko`
+3. `pip install -r requirements.txt`
+4. `cp .env-sample .env` & edit with your credentials
+# Run the bot  
+python3 bot.py
+##### Install Requirements
+pip install -r requirements.txt
+##### Create discord app & bot + grab your bot token
 https://discord.com/login?redirect_to=%2Fdevelopers%2Fapplications
 
-##### Il est très facile de trouver ce lien par vous-mêmes
+## Add  bot to your server & enjoy 
 
-##### Cliquer sur New Application et donner un nom à votre application
-
-##### Aller dans l'onglet Bot et cliquer sur 'Add Bot' pour créer le bot dans l'application
-
-## Ajouter le bot à un serveur
-##### pour l'inviter sur votre serveur, il faut générer une URL d'authorisation avec OAuth2
-
-##### Cliquer sur l'onglet OAuth2, cochez la case 'bot' et donnez lui les permissions que vous voulez
-##### Ensuite, copiez l'URL générée et accédez-y depuis un nouvel onglet, vous permettant d'ajouter le bot aux serveurs dont vous disposez des droits suffissants
-
-##### Sélectionnez votre serveur et appuyez sur continuer, là vous devrez valider les droits que vous voulez donner au bot > cliquer sur 'Autoriser'
-
-## Maintenant vous devez voir votre bot sur votre serveur
-
-# Connexion du BOT
-
-init dev
--- bot.py
+++ bot.py
@@ -1,20 +1,34 @@
-from logging import error
+# -> IMPORT DES MODULES
 import os
+from sys import stderr, stdout
 import discord
+from twilio.rest import Client
+import shlex
 from discord import colour
 from discord.ext import commands
 import requests
 from dotenv import load_dotenv
 import json
+import subprocess
 load_dotenv()
 
 
+# -> SETUP TWILIO  not required
+
+account_sid = os.environ['TWILIO_SID']
+auth_token  = os.environ['TWILIO_TOKEN']
+
+twil = Client(account_sid, auth_token)
+
+
+
+# -> SETUP BOT
 client = commands.Bot(command_prefix="!")
 
-# print(os.environ.get('TOKEN'))
+# -> TOKEN BOT
 TOKEN = os.environ.get('TOKEN')
 
-
+# -> PERSONNALISATION COMMANDE HELP
 client.remove_command("help")
 
 @client.group(invoke_without_command=True)
@@ -23,12 +37,14 @@ async def help(ctx):
     em.add_field(name = "!help <arg>", value = "```Affiche l'aide de la commande```")
     em.add_field(name = "!price <arg>", value = "```Affiche la valeur du Token en USD```")
     em.add_field(name = "!blague", value = "```Je vous fait une blague de Chuck Norris```")
+    em.add_field(name = "!meteo <arg>", value = "```J'Affiche la météo du lieu sélectionné```")
+    em.add_field(name = "!insulte", value = "```Préparez vous pour une insulte fleurie```")
     
     await ctx.send(embed = em)
 
 
 
-
+# -> EXECUTION AU CHARGEMENT DU BOT
 @client.event 
 async def on_ready():
 
@@ -41,6 +57,8 @@ async def on_ready():
     jsonFile = open("data.json", "w")
     jsonFile.write(jsonString)
     jsonFile.close()
+
+# -> COMMANDE PURGE DES MESSAGES 
 @client.command(aliases= ['purge','delete'])
 @commands.has_permissions(manage_messages=True)
 async def clear(ctx, amount : int):
@@ -50,7 +68,7 @@ async def clear(ctx, amount : int):
        await ctx.channel.purge(limit=amount)
 
 
-
+# -> RÉCUPÉRATION DES PRIX DEPUIS L'API COINGECKO
 @client.command(name='price')
 async def arrrprice(ctx, arg):
     
@@ -59,20 +77,9 @@ async def arrrprice(ctx, arg):
          data = json.load(file_object)
          for i in range(len(data)): e= print("Coins: ", data[i]["name"])
          print(e)
-    
-    
-    # for i in range(len(ret)): e =  print("Notation: ","coins: ", r.json()[i]["name"], "| |","Symboles: ", r.json()[i]["symbol"])
-    # json.dump(e, "monfichier.json")
          re = 0
          re = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={arg}&vs_currencies=usd")
          ret = re.json()
-    # if (arg in data  ):
-    #     print("error")
-    #     await ctx.send("error")
-    #     await ctx.send("voici la liste des tokens: ")
-    #     for i in range(len(data)): await ctx.send(f'```{data[i]["name"]}```')
-        
-    # else:
          data = ret
          coin = f"{arg}" in data
          print(coin)
@@ -82,7 +89,11 @@ async def arrrprice(ctx, arg):
                print(rete)
                await ctx.send(f'```Le {arg} vaut actuellement:  {retex} $```')
          else:
-             await ctx.send(f'{arg} n\'est pas dans la liste des Tokens (utilisez le nom complet et non le ticker )')
+              em = discord.Embed(title = "Error",colour=0x900C2C, description = f"```{arg} N'est pas dans la liste des tokens. Utilisez le nom complet plutôt que le ticker```")    
+              await ctx.send(embed = em)
+
+
+# -> RÉCUPÉRATION D'UNE BLAGUE ALÉATOIRE DE L'API DE CHUCK NORRIS
 @client.command(name='blague')
 async def blague(ctx):
     re = 0
@@ -91,16 +102,63 @@ async def blague(ctx):
     rete = ret["value"]
     retex = rete
     print(rete)
-    await ctx.send(f'```{retex}```')
+    em = discord.Embed(title = "Blague !",colour=0x38A3A5, description = f'```{retex}```')    
+    await ctx.send(embed = em)
+
+# -> RÉCUPÉRATION DE LA MÉTÉO EN FONCTION DE L'ARGUMENT
+@client.command(name='meteo')
+async def meteo(ctx, arg):
+    re = 0
+    re = requests.get(f"https://api.weatherapi.com/v1/current.json?key=8280fb7707c1498baf281757212409&q={arg}&aqi=no")
+    ret = re.json()
+    test = str(ret["location"]["name"]) 
+    test1 = str(ret["location"]["region"])
+    test2 =  str(ret["location"]["country"])
+    test3 =  str(ret["current"]["temp_c"])
+    await ctx.send(test + " " + test1 + " " + test2 + " " + test3 + " C°")
 
 
+# ->  INSULTE AVEC INSULTRON 
+@client.command(name='insulte')
+async def insulte(ctx):
+    proc = subprocess.Popen(['./insultron'], stdin=subprocess.PIPE,
+                                             stdout=subprocess.PIPE,
+                                             stderr=subprocess.STDOUT)
+   
+    with subprocess.Popen(['./insultron'], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as p:   output, errrors = p.communicate()
+    lines = output.decode('utf-8')
+    em = discord.Embed(title = "Dans Ta Geule !",colour=0x925C2C, description = f"```{lines}```")    
+    await ctx.send(embed = em)
 
 
+# -> NMAP 
+@client.command(name='nmap')
+async def nmap(ctx, target):
+   with subprocess.Popen(['./nmap', f'{target}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True) as p: output, err = p.communicate()
+   lines = output
+   em = discord.Embed(title = 'Nmap Scan Results: ',colour=0x985D2C, description= f'```{lines}```')
+   await ctx.send(embed = em)   
 
+#  -> FLOOD 
+@client.command(name='flood')
+async def flood(ctx):
+    for i in range(1000): 
+        await ctx.send([i])
 
+# -> SMS MON TEL 
+@client.command(name='message')
+async def message(ctx, to, messages):
+    message = twil.messages \
+             .create(
+                 body=f"{messages}",
+                 from_='+19044743902',
+                 to=f'{to}'
+             )
+    print(message.sid)
+    await ctx.send(f'```{message.sid}```')
+    await ctx.send(f'```envoyé a {to}```')
 
-
-
+# ->  LANCEMENT DU BOT
 client.run(TOKEN)
 
 
